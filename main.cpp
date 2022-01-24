1764bbd460e43dcce0eac9a64b0ce439514e6b04
Will <twitchifygamers@gmail.com>
2022-01-24 18:30:11 +0000

    Added class etc

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 #include <string>
 
+void isMyNameLong(const std::string& name);
+
+class Covid19 {
+public:
+    void doIHaveCovid() {
+        std::cout << "Probably";
+    }
+};
+
 int main() {
-    std::string firstname;
-    std::string surname;
+    std::basic_string<char> firstname;
+    std::basic_string<char> surname;
 
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Enter your first name: ";
@@ -12,5 +21,22 @@ int main() {
     std::cin >> surname;
     std::string fullname = firstname + " " + surname;
     std::cout << "Your name is " << fullname << std::endl;
+    isMyNameLong(firstname);
+    std::cout << "Memory address for " << firstname << " is " << &firstname << std::endl;
+    std::cout << "And for " << surname << " is " << &surname;
+    Covid19 ahh;
+    ahh.doIHaveCovid();
     return 0;
+}
+
+void isMyNameLong(const std::string& name)
+{
+    if (name.length() > 5)
+    {
+        std::cout << "You have a long name" << std::endl;
+    }
+    else
+    {
+        std::cout << "You have a short name" << std::endl;
+    }
 }
