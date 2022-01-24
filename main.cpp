65b875177375ecc48e0e0832d0fc0667b5668881
Will <twitchifygamers@gmail.com>
2022-01-24 20:03:15 +0000

    Added load of covid stuff funny

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <utility>
 
 void isMyNameLong(const std::string& name);
+void askForCovidTest();
 
 class Dog {
 public:
@@ -26,11 +27,31 @@ public:
 
 class Covid19 {
 public:
+    bool hasCovid = false;
+    bool hasVaccine = false;
+    bool hasTest = false;
+
     void doIHaveCovid() {
-        std::cout << "You probably have COVID!" << std::endl;
+        if (hasCovid)
+        {
+            std::cout << "You have COVID" << std::endl;
+        }
+        else if (!hasCovid)
+        {
+            std::cout << "You don't have COVID!" << std::endl;
+        }
     }
     void canIHaveAVaccine() {
-        std::cout << "Your not allowed a vaccine sorry." << std::endl;
+        if (hasVaccine)
+        {
+            std::cout << "You already had your vaccine silly!" << std::endl;
+            return;
+        }
+        else if (!hasVaccine)
+        {
+            std::cout << "Yeah sure!" << std::endl;
+            hasVaccine = true;
+        }
     }
     void isCovidCool() {
         std::cout << "COVID is not cool!" << std::endl;
@@ -55,7 +76,40 @@ int main() {
     ahh.doIHaveCovid();
     ahh.canIHaveAVaccine();
     ahh.isCovidCool();
+    askForCovidTest();
+
     return 0;
+}
+
+void askForCovidTest()
+{
+    std::string response;
+
+    std::cout << "Do you want a COVID test? Y/N" << std::endl;
+    std::cin >> response;
+    if (response == "Y")
+    {
+        Covid19 beep{};
+        if (beep.hasTest)
+        {
+            std::cout << "You already have a test!" << std::endl;
+        }
+        else if (!beep.hasTest)
+        {
+            std::cout << "Okay" << std::endl;
+            beep.hasTest = true;
+        }
+    }
+    else if (response == "N")
+    {
+        std::cout << "Bruh really!?" << std::endl;
+        return;
+    }
+    else
+    {
+        std::cout << "Enter valid input, either Y or N." << std::endl;
+        return;
+    }
 }
 
 void isMyNameLong(const std::string& name)
@@ -63,9 +117,11 @@ void isMyNameLong(const std::string& name)
     if (name.length() > 5)
     {
         std::cout << "You have a long name" << std::endl;
+        return;
     }
     else
     {
         std::cout << "You have a short name" << std::endl;
+        return;
     }
 }
