b414d82b01722d463e22ddb696089b927c42edd0
Will <twitchifygamers@gmail.com>
2022-01-24 17:47:34 +0000

    Lots of cool kid stuff

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <string>
 
 int main() {
+    std::string firstname;
+    std::string surname;
+
     std::cout << "Hello, World!" << std::endl;
-    std::cout << "My name is Will!" << std::endl;
+    std::cout << "Enter your first name: ";
+    std::cin >> firstname;
+    std::cout << "Enter your last name: ";
+    std::cin >> surname;
+    std::string fullname = firstname + " " + surname;
+    std::cout << "Your name is " << fullname << std::endl;
     return 0;
 }
